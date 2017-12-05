b1bf51cecac18ea493b95b3a7c046250a6c33eff
Will Pitts <will.pitts@outlook.com>
2017-12-04 19:45:34 -0800

    [D3/P2] Done.

--- day03/day3.lua
+++ day03/day3.lua
@@ -3,9 +3,19 @@ NORTH = 1
 WEST = 2
 SOUTH = 3
 
+Position = {}
+Position.__index = Position
+function Position:new(x, y)
+    return setmetatable({x = x, y = y}, self)
+end
+
+function Position:to_string()
+    return "{x=" .. self.x .. ",y=" .. self.y .. "}"
+end
+
 -- I know there's an O(1) formula that can calculate this, but I'm lazy and drinking.
 function distance_from(n)
-    local position = {x = 0, y = 0}
+    local position = Position:new(0, 0)
     local direction = EAST
     local turning_counter = 0
     local turning_counter_max = 1
@@ -32,10 +42,62 @@ function distance_from(n)
             direction = (direction + 1) % 4
         end
 
-        -- print(position.x .. " " .. position.y)
+        -- print(position:to_string())
     end
 
     return math.abs(position.x) + math.abs(position.y)
 end
 
-print(distance_from(325489))
+-- I know there's an O(1) formula that can calculate this, but I'm lazy and drinking.
+function value_larger_than(n)
+    local position = Position:new(0, 0)
+    local values = {}
+    values[position:to_string()] = 1
+    local direction = EAST
+    local turning_counter = 0
+    local turning_counter_max = 1
+    local high = false
+    local i = 1
+    while true do
+        if direction == EAST then
+            position.x = position.x + 1
+        elseif direction == NORTH then
+            position.y = position.y - 1
+        elseif direction == WEST then
+            position.x = position.x - 1
+        else
+            position.y = position.y + 1
+        end
+
+        local value = 0
+        for x = -1, 1 do
+            for y = -1, 1 do
+                local other_position = Position:new(position.x + x, position.y + y)
+                local other_position_key = other_position:to_string()
+                if values[other_position_key] then
+                    value = value + values[other_position_key]
+                end
+            end
+        end
+
+        if value > n then return value end
+        values[position:to_string()] = value
+
+        turning_counter = turning_counter - 1
+        if turning_counter < 1 then
+            if high then
+                turning_counter_max = turning_counter_max + 1
+            end
+
+            high = not high
+            turning_counter = turning_counter_max
+            direction = (direction + 1) % 4
+        end
+
+        print(position:to_string())
+    end
+
+    return values[position:to_string()]
+end
+
+print(value_larger_than(325489))
